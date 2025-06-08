468006e502c1afe3e31ffbb2c96a9f8a23eb1a4c
Chandra Tri Antomo <88928844+Chandra2004@users.noreply.github.com>
2025-06-08 13:10:45 +0700

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,6 +1,6 @@
 <?php
-    require_once __DIR__ . '/../vendor/autoload.php';
-    require_once __DIR__ . '/../app/Helpers/helpers.php';
+    require_once __DIR__ . '/vendor/autoload.php';
+    require_once __DIR__ . '/app/Helpers/helpers.php';
 
     define('ROOT_DIR', dirname(__DIR__));
 
