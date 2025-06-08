f6a896f1d22cddfbf35ac8cb94bc261454a294e9
Chandra Tri Antomo <88928844+Chandra2004@users.noreply.github.com>
2025-06-08 22:06:57 +0700

    Update CacheManager.php

--- app/App/CacheManager.php
+++ app/App/CacheManager.php
@@ -1,36 +1,65 @@
 <?php
-namespace {{NAMESPACE}}\App;
+    namespace {{NAMESPACE}}\App;
 
-class CacheManager
-{
-    public static function remember($key, $ttl, $callback)
-    {
-        $cacheDir = __DIR__ . '/../../../storage/cache/';
-        if (!is_dir($cacheDir)) {
-            mkdir($cacheDir, 0755, true);
+    class CacheManager {
+        protected static string $cacheDir = __DIR__ . '/../Storage/cache/';
+
+        /**
+         * Simpan cache dengan TTL (time-to-live)
+         */
+        public static function remember($key, $ttl, $callback)
+        {
+            self::ensureCacheDir();
+
+            $cacheKey = md5(is_array($key) ? json_encode($key) : $key);
+            $filePath = self::$cacheDir . $cacheKey . '.cache';
+
+            if (file_exists($filePath)) {
+                $data = json_decode(file_get_contents($filePath), true);
+                if (isset($data['expires_at']) && time() < $data['expires_at']) {
+                    return $data['value'];
+                }
+            }
+
+            $value = $callback();
+            $data = [
+                'value' => $value,
+                'expires_at' => time() + $ttl
+            ];
+
+            file_put_contents($filePath, json_encode($data));
+            return $value;
         }
 
-        $filePath = $cacheDir . md5($key) . '.cache';
+        /**
+         * Hapus 1 atau banyak cache berdasarkan key
+         */
+        public static function forget($key)
+        {
+            self::ensureCacheDir();
 
-        if (file_exists($filePath)) {
-            $data = json_decode(file_get_contents($filePath), true);
-            if (time() < $data['expires_at']) {
-                return $data['value'];
+            $keys = is_array($key) ? $key : [$key];
+            $success = true;
+
+            foreach ($keys as $k) {
+                $cacheKey = md5(is_array($k) ? json_encode($k) : $k);
+                $filePath = self::$cacheDir . $cacheKey . '.cache';
+
+                if (file_exists($filePath)) {
+                    $success = $success && unlink($filePath);
+                }
+            }
+
+            return $success;
+        }
+
+        /**
+         * Pastikan direktori cache tersedia
+         */
+        protected static function ensureCacheDir()
+        {
+            if (!is_dir(self::$cacheDir)) {
+                mkdir(self::$cacheDir, 0755, true);
             }
         }
-
-        $value = $callback();
-        $data = [
-            'value' => $value,
-            'expires_at' => time() + $ttl
-        ];
-        file_put_contents($filePath, json_encode($data));
-        return $value;
     }
-
-    public static function forget($key)
-    {
-        $filePath = __DIR__ . '/../../../storage/cache/' . md5($key) . '.cache';
-        return file_exists($filePath) && unlink($filePath);
-    }
-}
