453f8a8aef772fd1244af8fe5d32115e6fee4235
Chandra Tri Antomo <88928844+Chandra2004@users.noreply.github.com>
2025-06-29 06:20:12 +0700

    Update Logging.php

--- app/App/Logging.php
+++ app/App/Logging.php
@@ -13,7 +13,6 @@
                 self::$logger = new Logger('app');
                 self::$logger->pushHandler(new StreamHandler(__DIR__ . '/../Storage/logs/app.log', Logger::DEBUG));
                 
-                // Notifikasi Slack untuk error kritis
                 if ($webhook = Config::get('SLACK_WEBHOOK_URL')) {
                     self::$logger->pushHandler(new SlackWebhookHandler($webhook, Logger::ERROR));
                 }
