3f6db1dbbd5e2fecfca15b84bc0c2c6ca809c0bd
Chandra Tri Antomo <88928844+Chandra2004@users.noreply.github.com>
2025-06-29 21:29:19 +0700

    Create DebugController.php

---
+++ app/Http/Controllers/DebugController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace {{NAMESPACE}}\Http\Controllers;
+
+use {{NAMESPACE}}\App\{Config, Database, View, CacheManager};
+use {{NAMESPACE}}\Helpers\Helper;
+use Exception;
+
+class DebugController
+{
+    public static function showException(\Throwable $e)
+    {
+        http_response_code(500);
+        View::render('debug.exception', [
+            'class' => get_class($e),
+            'message' => $e->getMessage(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'trace' => $e->getTraceAsString(),
+        ]);
+    }
+
+    public static function showFatal(array $error)
+    {
+        http_response_code(500);
+        View::render('debug.fatal', [
+            'message' => $error['message'] ?? 'Fatal error',
+            'file' => $error['file'] ?? 'unknown',
+            'line' => $error['line'] ?? 'unknown',
+        ]);
+    }
+}
